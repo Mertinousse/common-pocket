d7d17226cc1a14c67d44e2f6a7364c07070b3ea8
mertinousse <martin@appmaintainers.com>
2020-10-06 09:33:30 +0200

    Style + ionicons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-%w[
-  Bevétel
-  Egészség 
-  Egyéb
-  Étel 
-  Háztartás
-  Közlekedés 
-  Kütyük 
-  Lakás 
-  Ruházat
-  Szépségápolás
-  Szórakozás 
-].each do |category|
-  Category.find_or_create_by(name: category)
+[
+  %w[Bevétel wallet],
+  %w[Egészség medkit],
+  %w[Egyéb receipt],
+  %w[Étel restaurant],
+  %w[Háztartás trash-bin],
+  %w[Közlekedés subway],
+  %w[Kütyük hardware-chip],
+  %w[Lakás home],
+  %w[Ruházat shirt],
+  %w[Szépségápolás flower],
+  %w[Szórakozás game-controller]
+].each do |category_params|
+  Category.find_or_create_by(name: category_params.first, key: category_params.second)
 end
