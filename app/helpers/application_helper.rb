c40e0ee888a3bf4fdf5be872f97d5f6ac21cea81
mertinousse <martin@appmaintainers.com>
2020-10-07 17:49:26 +0200

    Reduce flickering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def permanent
     {
       'data-reflex-permanent': true,
+      'data-turbolinks-permanent': true,
       id: "permanent-#{SecureRandom.uuid}"
     }
   end
