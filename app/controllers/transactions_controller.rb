f5b98f1317b88964eccf311e69b43e7e65fd2c00
mertinousse <martin@appmaintainers.com>
2020-10-07 10:45:57 +0200

    Fix n+1 query

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,7 @@
 class TransactionsController < ApplicationController
   def index
     @offset = params[:offset].to_i || 0
-    @transactions = Transaction.months_before(@offset)
+    @transactions = Transaction.months_before(@offset).includes(:category, :user)
   end
 
   def create
